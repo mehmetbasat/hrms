c63eaba40b3bc14a9c6b43b0833df0d78f4630e7
basat <mehmetbasatt@gmail.com>
2024-07-13 17:02:23 +0300

    User object added.

---
+++ src/main/java/project/hrms/entities/concretes/User.java
@@ -0,0 +1,38 @@
+package project.hrms.entities.concretes;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    @Email
+    @NotBlank(message = "Email cannot contain spaces")
+    @Column(name = "mail")
+    String mail;
+
+    @NotNull(message = "Password cannot be null")
+    @Column(name = "password")
+    String password;
+
+
+    @NotNull(message = "Password cannot be null")
+    @Column(name = "password_repeat")
+    String password_again;
+
+}
