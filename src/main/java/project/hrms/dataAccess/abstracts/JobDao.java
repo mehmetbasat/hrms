a4534a9b43854df453d826679714a58d83e383ca
basat <mehmetbasatt@gmail.com>
2024-07-13 16:59:15 +0300

    Method added.

--- src/main/java/project/hrms/dataAccess/abstracts/JobDao.java
+++ src/main/java/project/hrms/dataAccess/abstracts/JobDao.java
@@ -3,5 +3,8 @@ package project.hrms.dataAccess.abstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.hrms.entities.concretes.Job;
 
+import java.util.Optional;
+
 public interface JobDao extends JpaRepository<Job, Integer> {
+    Optional<Job> findJobByName(String title);
 }
