de6d8445ac0556a044bf8ad86d4522ba8e661296
basat <mehmetbasatt@gmail.com>
2024-07-13 17:00:39 +0300

    Job service writed with Result.

--- src/main/java/project/hrms/business/abstracts/JobService.java
+++ src/main/java/project/hrms/business/abstracts/JobService.java
@@ -1,10 +1,12 @@
 package project.hrms.business.abstracts;
 
+import project.hrms.core.utilities.results.DataResult;
+import project.hrms.core.utilities.results.Result;
 import project.hrms.entities.concretes.Job;
 
 import java.util.List;
 
 public interface JobService {
-    void add(Job job);
-    List<Job> getAll();
+    Result add(Job job);
+    DataResult<List<Job>> getAll();
 }
