d1539d9bf2d55d2ac657c6b134b1da99b1f13152
basat <mehmetbasatt@gmail.com>
2024-07-13 16:57:47 +0300

    Job object has been rewritten. The name in the table has been changed

--- src/main/java/project/hrms/entities/concretes/Job.java
+++ src/main/java/project/hrms/entities/concretes/Job.java
@@ -1,28 +1,24 @@
 package project.hrms.entities.concretes;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "job_titles")
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Job {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     int id;
 
-    @Column(name = "name")
+    @Column(name = "job_title")
     String name;
 
-    public Job() {
-
-    }
-
-    public Job(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
