80fe721a0079f696ec75535a35f20c593b1c2441
mehmetbasat <mehmetbasatt@gmail.com>
2024-07-24 17:30:59 +0300

    City entity added.

---
+++ src/main/java/project/hrms/entities/concretes/City.java
@@ -0,0 +1,22 @@
+package project.hrms.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "city_name")
+    String cityName;
+}
