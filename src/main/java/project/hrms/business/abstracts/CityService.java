2e393ca8a362df9b0de8180d0b6bd48e0d129be0
mehmetbasat <mehmetbasatt@gmail.com>
2024-07-24 17:38:50 +0300

    CityServiceadded.

---
+++ src/main/java/project/hrms/business/abstracts/CityService.java
@@ -0,0 +1,15 @@
+package project.hrms.business.abstracts;
+
+import project.hrms.core.utilities.results.DataResult;
+import project.hrms.core.utilities.results.Result;
+import project.hrms.dataAccess.abstracts.CityDao;
+import project.hrms.entities.concretes.City;
+
+import java.util.List;
+
+public interface CityService {
+
+    Result add(City city);
+    DataResult<List<City>> gelAll();
+
+}
