cf804a52b3c3da9ed6ba46453888985835f85d65
mehmetbasat <mehmetbasatt@gmail.com>
2024-07-24 17:41:53 +0300

    Job advertisement entity added.

---
+++ src/main/java/project/hrms/entities/concretes/JobAdvertisement.java
@@ -0,0 +1,59 @@
+package project.hrms.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "job_advertisements")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","products"})
+public class JobAdvertisement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "max_salary")
+    private int maxSalary;
+
+    @Column(name = "min_salary")
+    private int minSalary;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "end_date")
+    private Date endDate;
+
+    @Column(name = "status")
+    private boolean status;
+
+    @Column(name = "free_position_amount")
+    private int freePositionAmount;
+
+    @ManyToOne()
+    @JoinColumn(name = "employer_id")
+    private Employer employer;
+
+    @ManyToOne()
+    @JoinColumn(name="city_id")
+    private City city;
+
+    @ManyToOne()
+    @JoinColumn(name = "job_title_id")
+    private Job job;
+
+}
