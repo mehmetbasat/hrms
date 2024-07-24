a884fead4d3469f93801d584b4b7e9dbbe3f7226
mehmetbasat <mehmetbasatt@gmail.com>
2024-07-24 17:32:23 +0300

    CityManager entity added.

---
+++ src/main/java/project/hrms/business/concretes/CityManager.java
@@ -0,0 +1,35 @@
+package project.hrms.business.concretes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.hrms.business.abstracts.CityService;
+import project.hrms.core.utilities.results.DataResult;
+import project.hrms.core.utilities.results.Result;
+import project.hrms.core.utilities.results.SuccessDataResult;
+import project.hrms.core.utilities.results.SuccessResult;
+import project.hrms.dataAccess.abstracts.CityDao;
+import project.hrms.entities.concretes.City;
+
+import java.util.List;
+
+@Service
+public class CityManager implements CityService {
+
+    private CityDao cityDao;
+
+    @Autowired
+    public CityManager(CityDao cityDao) {
+        this.cityDao = cityDao;
+    }
+
+    @Override
+    public Result add(City city) {
+        cityDao.save(city);
+        return new SuccessResult("City added successfully");
+    }
+
+    @Override
+    public DataResult<List<City>> gelAll() {
+        return new SuccessDataResult<>(cityDao.findAll());
+    }
+}
